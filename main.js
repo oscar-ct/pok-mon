18a16eaf4fa1e97271f7c202d743d3fe7914878a
Oscar Castro <oscar.a.castro818@gmail.com>
2022-07-06 00:33:57 -0500

    feat: added search bar functionality searching for pokemon, displaying images only

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 
 const mapToDOM = (pokemon) => `<div>
-<img src="${pokemon.back_default}" style="height: 220px; ">
+<img src="${pokemon.front_default}" style="height: 220px; ">
 </div>`
 
 function searchPokemon (pokemon) {
     const request = {"url": "https://pokeapi.co/api/v2/pokemon/" + pokemon, "method": "GET"}
     $.ajax(request).done(function (data) {
-        // const pokemon = data.sprites.map(mapToDOM);
-        console.log(data.sprites);
+        const pokemon = data.sprites.other["official-artwork"]
+        console.log(data.sprites.other["official-artwork"]);
+        $('#output').append(mapToDOM(pokemon))
     });
 }
+
+$('#searchPokemon').click(function (e) {
+    e.preventDefault();
+    const pokemon = $('#pokemon').val()
+    searchPokemon(pokemon)
+});
