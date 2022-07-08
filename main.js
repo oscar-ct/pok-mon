7e1bf763d0f5a2932f83b8c5518c0577b0a98c90
Oscar Castro <oscar.a.castro818@gmail.com>
2022-07-07 23:59:24 -0500

    feat: created new function that displays multiple pokemon types

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 
 ////////////   Helper Functions   ///////////
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+function pokemonTypes (arrayList) {
+    if (arrayList.length <= 1) {
+        return arrayList[0].type.name.charAt(0).toUpperCase() + arrayList[0].type.name.slice(1)
+    } else if (arrayList.length === 2) {
+        return arrayList[0].type.name.charAt(0).toUpperCase() + arrayList[0].type.name.slice(1) + ',' + arrayList[1].type.name.charAt(0).toUpperCase() + arrayList[1].type.name.slice(1)
+    }
 }
-
 
 ////////////  DOM  ///////////////
 const mapToDOM = (pokemon) => `<div>
 
 <img src="${pokemon.sprites.other["official-artwork"].front_default}" style="height: 185px; ">
 <div><h6>Name: <span>${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</span></h6></div>
-<div><h6>Type: <span>${pokemon.types[0].type.name.charAt(0).toUpperCase() + pokemon.types[0].type.name.slice(1)}</span></h6></div>
+<div><h6>Type: <span></span>${pokemonTypes(pokemon.types)}</h6></div>
 <div><h6>ID: <span>${pokemon.id}</span></h6></div>
+<button class="btn-test" onclick="test(${pokemon.id})">Test</button>
 </div>`
 
 ///////////   API request here  //////////////
@@ -34,12 +38,19 @@ $('#searchPokemon').click(function (e) {
 //////////  View All Button  /////////
 
 $('#viewAllPokemon').click(function () {
-    viewAllPokemon()
+    viewAllPokemon();
 });
 
 function viewAllPokemon () {
-    for (let i = 0; i <= 250; i++) {
+    for (let i = 0; i <= 151; i++) {
         searchPokemon(i)
     }
 }
 
+
+//////////// Test Button  /////////////
+
+
+function test(id) {
+    alert(id)
+}
