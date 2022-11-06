4613495d505d076450b8a091f78033c4f47d6473
Oscar Castro <oscar.a.castro818@gmail.com>
2022-11-06 17:16:20 -0600

    testing touchend evevnt listener

--- main.js
+++ main.js
@@ -154,7 +154,7 @@ const mapLocalPokemon = () => {
 
 
 
-$('#pokemon').on('input', function (e) {
+$('#pokemon').on('keyup touchend', function (e) {
     // $('.gen:checked').prop('checked', false);
     // console.log(localStoredPokemon);
     const searchTerm = $('#pokemon').val();
